2ec495dbd5ec444d98b25f8cbf3ecf11e2576cd2
Zubair Khalid <zubairkhalidce@gmail.com>
2023-05-11 16:21:52 +0100

    added react query client

--- frontend/app/src/index.jsx
+++ frontend/app/src/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from "react-query";
 import './styles/index.css';
 import App from './App';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
