0e4521bb74114d63949620bb3922c3a19a2c6cd5
Zubair Khalid <zubairkhalidce@gmail.com>
2023-05-18 10:52:31 +0100

    removed protfolio fetching logic, updated page imports

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,44 +1,19 @@
 import React from 'react';
-import axios from "axios";
-import { ethers } from 'ethers';
-import { useQuery } from "react-query";
 import { Route, Routes} from 'react-router-dom';
-import Home from './components/Home';
-import BuyCoffee from './components/BuyCoffee';
-import NotFound from './components/NotFound';
+import Home from './pages/Home';
+import BuyMeCoffee from './pages/BuyMeCoffee';
+import NotFound from './pages/NotFound';
 import contract from './assets/json/portfolio.json';
 
 function App() {
-
-
-  const fetchPortfolio = async () => {
-
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const portfolioContract = new ethers.Contract(contract.address, contract.abi, provider.getSigner());
-    
-    const portfolioURL = await portfolioContract.url();
-
-    const { data } = await axios.get(portfolioURL);
-    return data;
-  };
-
-  const { 
-    isLoading, 
-    isSuccess, 
-    error, 
-    isError, 
-    data: portfolio 
- } = useQuery("portfolio", fetchPortfolio);
-
-  if(isError) console.log(error);
 
   return (
 
     <main>
       <Routes>
         <Route path="*" element={<NotFound/>}></Route>
-        <Route path='/' element={<Home portfolio={portfolio}/>} exact />
-        <Route path='/buy-me-a-coffee' element={<BuyCoffee/>} />
+        <Route path='/' element={<Home contract={contract}/>} exact />
+        <Route path='/buy-me-a-coffee' element={<BuyMeCoffee contract={contract}/>} />
       </Routes>
     </main>
   );
